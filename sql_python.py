821f0f40c7779e9f05f96e1a9ac8e26a36fca083
esther <estherkc100@gmail.com>
2019-11-05 19:55:53 +0300

    added content to sql_python file

--- sql_python.py
+++ sql_python.py
@@ -5,3 +5,33 @@ import pymysql  # connects to the database
 connection = pymysql.connect("localhost", "root", "", "modcom_db")
 df = pandas.read_sql("SELECT * FROM `TABLE 5`", connection)
 print(df)
+print(df['Property_Area'])
+
+# generating a pie chart
+
+df['Self_Employed'].fillna('Not Known', inplace=True)
+df.groupby('Self_Employed').size().plot(kind = 'pie', autopct = '%1.1f%%',
+                                            explode=(0, 0.2, 0))
+
+plt.title("Self employed distribution")
+plt.xlabel("")
+plt.ylabel("")
+plt.show()
+
+
+
+# histogram
+figure, ax = plt.subplots()
+ax.hist(df['ApplicantIncome'], color = 'green', label = 'App.Income', alpha = 0.75)
+ax.hist(df['CoapplicantIncome'], color = 'red', label = 'C. inc', alpha = 0.75)
+
+ax.legend(loc = 'best')
+ax.set_title('Distribution of Applicant Income')
+ax.set_xlabel('Income')
+ax.set_ylabel('Freq')
+plt.show()
+
+
+#stacked bar chart
+
+
